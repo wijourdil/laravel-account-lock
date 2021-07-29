edb30905f2c10911781b8b59a8d01c454b8f894d
wijourdil <wijourdil@users.noreply.github.com>
2021-07-29 21:39:07 +0000

    :art: Fix styling

--- src/Events/AccountLocked.php
+++ src/Events/AccountLocked.php
@@ -9,7 +9,9 @@ use Illuminate\Queue\SerializesModels;
 
 class AccountLocked
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     public function __construct(private Authenticatable $authenticatable)
     {
